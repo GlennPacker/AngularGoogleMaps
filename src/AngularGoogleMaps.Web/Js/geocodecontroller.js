84af67885490c358df2df851ebfa59743d3f1428
glenn packer <glenn.packer@yahoo.co.uk>
2015-07-22 23:43:52 +0100

    starting geo

--- src/AngularGoogleMaps.Web/Js/geocodecontroller.js
+++ src/AngularGoogleMaps.Web/Js/geocodecontroller.js
@@ -2,26 +2,34 @@
 
 var cont = geocont.controller("geocodecontroller", function ($scope, uiGmapGoogleMapApi) {
     $scope.markers = []
-
-    
+    $scope.location = { "k": 50.888169, "D": 0.26173930000004475 };
+    $scope.lat = 50.888169
+    $scope.lon = 0.26173930000004475
+    $scope.loc = { latitude: 10.888169, longitude: 0.26173930000004475 }
 
 
     uiGmapGoogleMapApi.then(function (maps) {
         $scope.map = {
-            center: { latitude: 0, longitude: -73 },
             streetViewControl: true,
-            zoom: 8
+            zoom: 15
         };
 
         var geocoder = new google.maps.Geocoder();
-            geocoder.geocode({ "address": '9 chichester road hellingly bn 27 4bf' }, function (results, status) {
-                if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
-                    var location = results[0].geometry.location;
-                    $scope.map.panTo(location);
-                }
-            });
-       
+        geocoder.geocode({ "address": '9 chichester road hellingly bn 27 4bf' }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+                $scope.loc = { latitude: results[0].geometry.location.k, longitude: results[0].geometry.location.D };
+            }
+        });
     });
+
+    $scope.Centre = function () {
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ "address": $scope.address }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+                $scope.loc = { latitude: results[0].geometry.location.k, longitude: results[0].geometry.location.D };
+            }
+        });
+    }
 
     $scope.message = "hello";
 });
